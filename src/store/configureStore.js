fc12c633fc928ebcfcde780c621fab2e7ac4d4f9
Xue Yu <xueyublue@gmail.com>
2021-04-17 22:05:33 +0800

    added session store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,16 @@
 import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
-import instance from "./oracle/instance";
-import space from "./oracle/space";
 import api from "./middleware/api";
 import logger from "./middleware/logger";
 import toast from "./middleware/toast";
+import instance from "./oracle/instance";
 import instanceUI from "./ui/instance";
 import sidenavUI from "./ui/sidenav";
+import space from "./oracle/space";
 import spaceUI from "./ui/space";
-import userUI from "./ui/user";
 import user from "./oracle/user";
+import userUI from "./ui/user";
+import session from "./oracle/session";
+import sessionUI from "./ui/session";
 
 export default function store() {
   return configureStore({
@@ -17,12 +19,14 @@ export default function store() {
         space: space,
         instance: instance,
         user: user,
+        session: session,
       }),
       ui: combineReducers({
         sidenav: sidenavUI,
         instance: instanceUI,
         space: spaceUI,
         user: userUI,
+        session: sessionUI,
       }),
     }),
     middleware: [...getDefaultMiddleware(), logger({ destination: "console" }), toast, api],
---
+++ src/store/oracle/session.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+import moment from "moment";
+import { apiCallBegan } from "../api";
+
+const slice = createSlice({
+  name: "session",
+  initialState: {
+    list: [],
+    loading: false,
+    lastFetch: null,
+  },
+  reducers: {
+    sessionsRequested: (state, action) => {
+      state.loading = true;
+    },
+    sessionsReceived: (state, action) => {
+      state.list = action.payload;
+      state.loading = false;
+      state.lastFetch = Date.now();
+    },
+    sessionsRequestFailed: (state, action) => {
+      state.loading = false;
+    },
+  },
+});
+
+const { sessionsRequested, sessionsReceived, sessionsRequestFailed } = slice.actions;
+
+const url = "/oracle/sessions";
+
+// Load Sessions
+export const loadProfiles = () => (dispatch, getState) => {
+  const { lastFetch } = getState().oracle.sessions;
+  const diffInMinutes = moment().diff(moment(lastFetch), "minutes");
+  if (diffInMinutes < 5) return;
+  dispatch(
+    apiCallBegan({
+      url: url,
+      onStart: sessionsRequested.type,
+      onSuccess: sessionsReceived.type,
+      onError: sessionsRequestFailed.type,
+    })
+  );
+};
+
+export default slice.reducer;
