342e23da91a07cd2d26c3c85920b0dee180e5420
Xue Yu <xueyublue@gmail.com>
2021-04-17 20:33:24 +0800

    udpated dashboard

--- src/components/pages/Dashboard.jsx
+++ src/components/pages/Dashboard.jsx
@@ -1,10 +1,29 @@
+import { Grid } from "@material-ui/core";
 import React from "react";
+import InstanceDetails from "./instance/InstanceDetails";
+import SgaConfiguration from "./instance/SgaConfiguration";
+import Tablespace from "./space/Tablespace";
+import SgaConfigurationChart from "./instance/SgaConfigurationChart";
 
 //-------------------------------------------------------------
 // COMPONENT START
 //-------------------------------------------------------------
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <Grid container spacing={1}>
+        <Grid item xs={12} md={6}>
+          <InstanceDetails />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <SgaConfigurationChart />
+        </Grid>
+        <Grid item xs={12}>
+          <Tablespace />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Dashboard;
