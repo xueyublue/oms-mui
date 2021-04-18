72656c1a962a3309e82923e64007b00429dc32e0
Xue Yu <xueyublue@gmail.com>
2021-04-18 11:11:36 +0800

    completed session page

--- src/components/pages/session/Sessions.jsx
+++ src/components/pages/session/Sessions.jsx
@@ -6,15 +6,23 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import { Button, TablePagination, Box } from "@material-ui/core";
+import { Button, Grid, Switch, TablePagination } from "@material-ui/core";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
 
 import { TableCellHeader } from "../../common/TableCellHeader";
-import { pageChanged, pageSizeChanged } from "../../../store/ui/session";
+import { statusChanged, pageChanged, pageSizeChanged, showAllColumnsChanged } from "../../../store/ui/session";
 import { loadSessions } from "../../../store/oracle/session";
 
 const useStyles = makeStyles((theme) => ({
   table: {
     minWidth: 650,
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    width: 180,
   },
 }));
 
@@ -30,9 +38,18 @@ const Sessions = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
   const tableData = useSelector((state) => state.oracle.session.list);
+  const selectedStatus = useSelector((state) => state.ui.session.selectedStatus);
+  const showAllColumns = useSelector((state) => state.ui.session.showAllColumns);
   const pageSize = useSelector((state) => state.ui.session.pageSize);
   const currentPage = useSelector((state) => state.ui.session.currentPage);
 
+  const handleStatusChange = (event) => {
+    dispatch(statusChanged({ selectedStatus: event.target.value }));
+    dispatch(pageChanged({ currentPage: 0 }));
+  };
+  const handleShowAllColumnsChange = (event) => {
+    dispatch(showAllColumnsChanged({ showAllColumns: event.target.checked }));
+  };
   const handlePageChange = (event, newPage) => {
     dispatch(pageChanged({ currentPage: newPage }));
   };
@@ -43,11 +60,27 @@ const Sessions = () => {
 
   return (
     <>
-      <Button>Show All Columns</Button>
+      <Grid container alignItems="flex-end">
+        <Grid item>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="label-status">Status</InputLabel>
+            <Select labelId="label-status" id="select-status" value={selectedStatus} onChange={handleStatusChange}>
+              {["All", "Active", "Inactive"].map((status) => (
+                <MenuItem dense={true} value={status} key={status} key={status}>
+                  {status}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item>
+          <Switch checked={showAllColumns} onChange={handleShowAllColumnsChange} /> Show All Columns
+        </Grid>
+      </Grid>
       <TablePagination
         rowsPerPageOptions={[10, 15, 30, 100, 500]}
         component="div"
-        count={tableData.length}
+        count={tableData.filter((row) => (selectedStatus === "All" ? true : row.status === selectedStatus)).length}
         rowsPerPage={pageSize}
         page={currentPage}
         onChangePage={handlePageChange}
@@ -69,42 +102,45 @@ const Sessions = () => {
             <TableCellHeader>Process</TableCellHeader>
             <TableCellHeader>Program</TableCellHeader>
             <TableCellHeader>Client&nbsp;Info</TableCellHeader>
-            <TableCellHeader>Action</TableCellHeader>
-            <TableCellHeader>Module</TableCellHeader>
             <TableCellHeader>Logon&nbsp;Time</TableCellHeader>
-            <TableCellHeader>Server</TableCellHeader>
-            <TableCellHeader>Type</TableCellHeader>
-            <TableCellHeader>SQL&nbsp;Address</TableCellHeader>
-            <TableCellHeader>Previous&nbsp;SQL&nbsp;Address</TableCellHeader>
-            <TableCellHeader>SQL&nbsp;ID</TableCellHeader>
-            <TableCellHeader>Previous&nbsp;SQL&nbsp;ID</TableCellHeader>
+            {showAllColumns && <TableCellHeader>Action</TableCellHeader>}
+            {showAllColumns && <TableCellHeader>Module</TableCellHeader>}
+            {showAllColumns && <TableCellHeader>Server</TableCellHeader>}
+            {showAllColumns && <TableCellHeader>Type</TableCellHeader>}
+            {showAllColumns && <TableCellHeader>SQL&nbsp;Address</TableCellHeader>}
+            {showAllColumns && <TableCellHeader>Previous&nbsp;SQL&nbsp;Address</TableCellHeader>}
+            {showAllColumns && <TableCellHeader>SQL&nbsp;ID</TableCellHeader>}
+            {showAllColumns && <TableCellHeader>Previous&nbsp;SQL&nbsp;ID</TableCellHeader>}
           </TableRow>
         </TableHead>
         <TableBody>
-          {tableData.slice(currentPage * pageSize, currentPage * pageSize + pageSize).map((row, index) => (
-            <TableRow key={row.id}>
-              <TableCell align="center">{index + currentPage * pageSize + 1}</TableCell>
-              <TableCell>{row.id}</TableCell>
-              <TableCell>{row.serialNo}</TableCell>
-              <TableCell>{row.userName}</TableCell>
-              <TableCell>{row.status}</TableCell>
-              <TableCell>{row.osUser}</TableCell>
-              <TableCell>{row.machine}</TableCell>
-              <TableCell>{row.terminal}</TableCell>
-              <TableCell>{row.process}</TableCell>
-              <TableCell>{row.program}</TableCell>
-              <TableCell>{row.clientInfo}</TableCell>
-              <TableCell>{row.action}</TableCell>
-              <TableCell>{row.module}</TableCell>
-              <TableCell>{row.logonTime}</TableCell>
-              <TableCell>{row.server}</TableCell>
-              <TableCell>{row.type}</TableCell>
-              <TableCell>{row.sqlAddress}</TableCell>
-              <TableCell>{row.previousSqlAddress}</TableCell>
-              <TableCell>{row.sqlId}</TableCell>
-              <TableCell>{row.previousSqlId}</TableCell>
-            </TableRow>
-          ))}
+          {tableData
+            .filter((row) => (selectedStatus === "All" ? true : row.status === selectedStatus))
+            .slice(currentPage * pageSize, currentPage * pageSize + pageSize)
+            .map((row, index) => (
+              <TableRow key={row.id}>
+                <TableCell align="center">{index + currentPage * pageSize + 1}</TableCell>
+                <TableCell>{row.id}</TableCell>
+                <TableCell>{row.serialNo}</TableCell>
+                <TableCell>{row.userName}</TableCell>
+                <TableCell>{row.status}</TableCell>
+                <TableCell>{row.osUser}</TableCell>
+                <TableCell>{row.machine}</TableCell>
+                <TableCell>{row.terminal}</TableCell>
+                <TableCell>{row.process}</TableCell>
+                <TableCell>{row.program}</TableCell>
+                <TableCell>{row.clientInfo}</TableCell>
+                <TableCell>{row.logonTime}</TableCell>
+                {showAllColumns && <TableCell>{row.action}</TableCell>}
+                {showAllColumns && <TableCell>{row.module}</TableCell>}
+                {showAllColumns && <TableCell>{row.server}</TableCell>}
+                {showAllColumns && <TableCell>{row.type}</TableCell>}
+                {showAllColumns && <TableCell>{row.sqlAddress}</TableCell>}
+                {showAllColumns && <TableCell>{row.previousSqlAddress}</TableCell>}
+                {showAllColumns && <TableCell>{row.sqlId}</TableCell>}
+                {showAllColumns && <TableCell>{row.previousSqlId}</TableCell>}
+              </TableRow>
+            ))}
         </TableBody>
       </Table>
     </>
--- src/store/ui/session.js
+++ src/store/ui/session.js
@@ -4,12 +4,20 @@ const slice = createSlice({
   name: "session",
   initialState: {
     currentTab: 0,
-    pageSize: 15,
+    selectedStatus: "All",
+    showAllColumns: false,
+    pageSize: 30,
     currentPage: 0,
   },
   reducers: {
     setCurrentTab: (state, action) => {
       state.currentTab = action.payload.currentTab;
+    },
+    statusChanged: (state, action) => {
+      state.selectedStatus = action.payload.selectedStatus;
+    },
+    showAllColumnsChanged: (state, action) => {
+      state.showAllColumns = action.payload.showAllColumns;
     },
     pageSizeChanged: (state, action) => {
       state.pageSize = action.payload.pageSize;
@@ -20,5 +28,5 @@ const slice = createSlice({
   },
 });
 
-export const { setCurrentTab, pageSizeChanged, pageChanged } = slice.actions;
+export const { setCurrentTab, statusChanged, showAllColumnsChanged, pageSizeChanged, pageChanged } = slice.actions;
 export default slice.reducer;
