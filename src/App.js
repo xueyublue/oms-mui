ad29183c3111f3073ef90b00c8e26ee7c9a8a074
Xue Yu <xueyublue@gmail.com>
2021-04-23 23:05:35 +0800

    removed app container

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import AppContainer from "./components/AppContainer";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
+import { makeStyles } from "@material-ui/core/styles";
+import Appbar from "./components/Appbar";
+import Sidenav from "./components/Sidenav";
+import MainContainer from "./components/MainContainer";
 
 const store = configureStore();
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+}));
 
 //-------------------------------------------------------------
 // COMPONENT START
 //-------------------------------------------------------------
 function App() {
+  const classes = useStyles();
+
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <Router>
-          <AppContainer />
+          <div className={classes.root}>
+            <Appbar />
+            <Sidenav />
+            <MainContainer />
+          </div>
         </Router>
       </ThemeProvider>
     </Provider>
